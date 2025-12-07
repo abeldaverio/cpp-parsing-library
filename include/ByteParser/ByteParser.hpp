8e79068aeabb57a78d2bbf60b1193ea18f228884
abeldaverio <abel.daverio@epitech.eu>
2025-12-07 21:19:14 +0100

    feat(byte): map parser

--- include/ByteParser/ByteParser.hpp
+++ include/ByteParser/ByteParser.hpp
@@ -8,6 +8,8 @@
 #include <functional>
 #include <stdint.h>
 #include <sys/types.h>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 template <typename Type>
@@ -27,6 +29,26 @@ Parser<std::vector<Type>> parseByteArray(Parser<Type> p) {
   return parseByte<u_int32_t>() >>=
          std::function<Parser<std::vector<Type>>(u_int32_t)>(
              [p](u_int32_t nb) { return p * nb; });
+}
+
+template <typename Key, typename Value>
+Parser<std::unordered_map<Key, Value>> parseBytePair(Parser<Key> k,
+                                                     Parser<Value> v) {
+  return apply([](Key const &key,
+                  Value const &value) { return std::make_pair(key, value); },
+               k, v);
+}
+
+template <typename Key, typename Value>
+Parser<std::unordered_map<Key, Value>> parseByteMap(Parser<Key> k,
+                                                    Parser<Value> v) {
+  return apply(
+      [](std::vector<std::pair<Key, Value>> v) {
+        return std::unordered_map<Key, Value>(v.begin(), v.end());
+      },
+      parseByteArray(
+          apply([k, v](Key key, Value val) { return std::make_pair(key, val); },
+                k, v)));
 }
 
 Parser<std::string> parseByteString();
