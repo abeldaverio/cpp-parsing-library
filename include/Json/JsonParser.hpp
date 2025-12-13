22319d8125083729c1ec59a3f3a9450e9f6d3d4d
abeldaverio <abel.daverio@epitech.eu>
2025-12-13 16:47:34 +0100

    feat(json): json value constructors

--- include/Json/JsonParser.hpp
+++ include/Json/JsonParser.hpp
@@ -7,26 +7,27 @@
 #include <variant>
 #include <vector>
 
-
 struct JsonValue;
 
-using JsonArray  = std::vector<JsonValue>;
+using JsonArray = std::vector<JsonValue>;
 using JsonObject = std::unordered_map<std::string, JsonValue>;
 
-using JsonVariant = std::variant<
-    int,
-    double,
-    std::string,
-    bool,
-    JsonObject,
-    JsonArray
->;
+using JsonVariant =
+    std::variant<int, double, std::string, bool, JsonObject, JsonArray>;
 
 struct JsonValue {
-    JsonVariant value;
+  JsonValue(JsonVariant const v) : value(v) {}
+  JsonValue(int v) : value(v) {}
+  JsonValue(double v) : value(v) {}
+  JsonValue(const char *v) : value(std::string(v)) {}
+  JsonValue(std::string v) : value(std::move(v)) {}
+  JsonValue(bool v) : value(v) {}
+
+  JsonVariant value;
 };
 
 Parser<JsonValue> parseJsonValue();
+Parser<JsonArray> parseJsonArray();
 Parser<std::pair<std::string, JsonValue>> parseJsonLine();
 Parser<JsonObject> parseJsonObject();
 Parser<JsonObject> parseJsonRoot();
